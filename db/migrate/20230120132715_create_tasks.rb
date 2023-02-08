f9656023d78bbe443e3da871a39590f4d89f22ab
ChristoforosG <xristoforosgav@gmail.com>
2023-02-09 00:14:09 +0100

    Apply Create Task Table Migration

--- db/migrate/20230120132715_create_tasks.rb
+++ db/migrate/20230120132715_create_tasks.rb
@@ -2,6 +2,7 @@ class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
       t.string :title
+      t.string :category
       t.text :description
       t.integer :status
       t.datetime :deadline
